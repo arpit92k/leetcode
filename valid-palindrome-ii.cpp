cc6f59a85b1daae9d87c6ec8b73e7ec78c72b5e3
Arpit <arpit92k@gmail.com>
2019-07-06 11:34:02 +0530

    Create valid-palindrome-ii.cpp

---
+++ valid-palindrome-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    bool isPalindrome(string str, int s, int e)
+    {
+        while(s<e)
+        {
+            if(str[s] == str[e])
+            {
+                ++s;
+                --e;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string str) {
+        int s = 0;
+        int e = str.length() - 1;
+        while(s < e)
+        {
+            if(str[s] == str[e])
+            {
+                ++s;
+                --e;
+            }
+            else if(isPalindrome(str, s+1, e) || isPalindrome(str, s, e-1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+   
+};
